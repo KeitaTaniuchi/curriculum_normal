b896ce9040a23c2b82cdc806871d0615a3b4f672
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-04-13 21:52:57 +0900

    課題4 userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :last_name, presence: true, length: { maximum: 525 }
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 3 }
 end
