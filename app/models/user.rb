51f0620ff00fefd9861119de47203a3313422820
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-04-17 21:26:08 +0900

    課題4 パスワードのバリデーションを、sorcery公式のものに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :last_name, presence: true, length: { maximum: 525 }
+  validates :last_name, presence: true, length: { maximum: 255 }
   validates :first_name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+
+  # sorceryの公式ドキュメントに書かれているバリデーション
+  # https://github.com/Sorcery/sorcery/wiki/Simple-Password-Authentication
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
