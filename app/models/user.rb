67def1e1754a8904b6be29fa69cd48e34e26d5cc
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-04-24 21:55:36 +0900

    課題8 boardモデル・テーブルを作成し、userテーブルとのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :boards
+
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
