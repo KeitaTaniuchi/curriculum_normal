9b1c3e6db5abb2c08df7a8d13dfeae4d838a764c
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-04-24 23:08:31 +0900

    課題8 seedファイルを修正

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,10 +1,11 @@
 puts 'posts ...'
 
-User.limit(10).each do |user|
-  user.boards.create!(
-    title: Faker::Name.unique.name,
-    body: Faker::Hacker.say_something_smart
-  )
-
-  puts "posts = #{Board.count}"
+2.times do |n|
+  User.limit(10).each do |user|
+    user.boards.create!(
+      title: Faker::Name.unique.name,
+      body: Faker::Hacker.say_something_smart
+    )
+    puts "posts = #{Board.count}"
+  end
 end
