bae80de2e2e51f25680482ca173471e8cc3d1ad5
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-04-30 22:15:59 +0900

    課題8 seedファイルを修正

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,5 +1,14 @@
-puts 'users ...'
+puts 'Start creating administrator ...'
+User.create!(
+  last_name: "谷内",
+  first_name: "圭太",
+  email: "barbaakonta1995@gmail.com",
+  password: "keita1995",
+  password_confirmation: "keita1995"
+)
+puts '✨Finish creating administrator!!'
 
+puts 'Start creating users ...'
 10.times do |n|
   User.create!(
     last_name: Faker::Name.unique.last_name,
@@ -8,6 +17,6 @@ puts 'users ...'
     password: "test",
     password_confirmation: "test"
   )
-
   puts "users = #{User.count}"
 end
+puts '✨Finish creating users!!'
