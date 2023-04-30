1e4d4f11373aa21801f01bf20709cb1e7076ffcb
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-05-01 08:11:19 +0900

    課題9 掲示板作成機能を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,25 @@
 class BoardsController < ApplicationController
   def index
-    @boards = Board.all.includes(:user).order(created_at: :asc)
+    @boards = Board.all.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.new(board_params)
+    if @board.save
+      redirect_to boards_path, success: I18n.t('.flash.create_board_success')
+    else
+      flash.now[:danger] = I18n.t('.flash.create_board_failed')
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
   end
 end
