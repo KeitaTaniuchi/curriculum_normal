fc8461cc93c9eb9f07ae845a8be0535ed4f0600c
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-05-13 18:37:19 +0900

    課題11 プレビュー表示用のjsコードを作成

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,14 @@
+previewImage = () => {
+  const target = this.event.target;
+  const file = target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    const preview = document.querySelector("#preview");
+    if (preview) {
+      preview.src = reader.result;
+    }
+  };
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+};
