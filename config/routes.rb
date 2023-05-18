a21edc138113e45b38bed72cafa0380cb46ed777
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-05-19 07:18:12 +0900

    課題15 ブックマーク機能用のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   post   '/login',   to: 'user_sessions#create'
   delete '/logout',  to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
-
   resources :boards do
     resources :comments, only: %i[create], shallow: true
+    # /users/bookmarksにアクセスすると、userコントローラのbookmarksアクションが実行される
+    collection do
+      get 'bookmarks'
+    end
   end
+  resources :bookmarks, only: %i[create destroy]
+  resources :users, only: %i[new create]
 end
