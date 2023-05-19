c2896acc4403d9d1235da94ab8dfc6a5c7f7d5a6
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-05-19 22:00:53 +0900

    課題15 解答例を参考に修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,13 @@
 class BookmarksController < ApplicationController
-  def index
-    @bookmark_boards = current_user.bookmark_boards
-  end
-
   def create
     board = Board.find(params[:board_id])
     current_user.bookmark(board)
-    redirect_to boards_path, success: I18n.t('.flash.bookmarked')
+    redirect_back fallback_location: root_path, success: I18n.t('.flash.bookmarked')
   end
 
   def destroy
     board = current_user.bookmarks.find(params[:id]).board
     current_user.unbookmark(board)
-    redirect_to boards_path, success: I18n.t('.flash.bookmark_removed')
+    redirect_back fallback_location: root_path, success: I18n.t('.flash.bookmark_removed')
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :board
+
+  # user_idとboard_idの組み合わせが一意であることを確認(同じuser_idとboard_idの組み合わせが複数回出現しないようにする)
+  validates :user_id, uniqueness: { scope: :board_id }
 end
