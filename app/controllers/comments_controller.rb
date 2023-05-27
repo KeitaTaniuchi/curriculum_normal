4684c9acb9ff95129c3fe64b7d3578ef867becf7
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-05-27 10:17:24 +0900

    課題17 comemntsコントローラのcreateアクションからredirect_toを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to board_path(comment.board), success: I18n.t('.flash.created', item: Comment.human_attribute_name(:body))
-    else
-      redirect_to board_path(comment.board), danger: I18n.t('.flash.not_created', item: Comment.human_attribute_name(:body))
-    end
+    comment.save
   end
 
   private
