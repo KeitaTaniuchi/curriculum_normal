b903ee642d45fb2765ddd409523bcddcef430376
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-06-13 19:34:51 +0900

    課題21 password_resetコントローラを作成

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,32 @@
+class PasswordResetsController < ApplicationController
+  # sorceryのReset-passwordのドキュメントを参考にしている
+  # https://github.com/Sorcery/sorcery/wiki/Reset-password
+  skip_before_action :require_login
+
+  def new; end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, success: 'パスワードリセット手順を送信しました'
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+    not_authenticated if @user.blank?
+  end
+
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+    not_authenticated if @user.blank?
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, success: 'パスワードを変更しました'
+    else
+      render :edit
+    end
+  end
+end
