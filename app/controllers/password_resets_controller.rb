5bc21097cec54d0228890e3bda95af92b2f0fdf0
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-06-13 21:52:31 +0900

    課題21 解答例のコードとコメントを追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,20 +7,23 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
+    # 有効期限付きのリセットコードを生成し、ユーザーにメールを送信する
     @user&.deliver_reset_password_instructions!
     redirect_to login_path, success: 'パスワードリセット手順を送信しました'
   end
 
   def edit
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
+    # トークンでユーザーを検索し、有効期限切れもチェックします。
+    # トークンが見つかり、有効であればそのユーザーを返します。
+    @user = User.load_from_reset_password_token(@token)
     not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-    not_authenticated if @user.blank?
+    @user = User.load_from_reset_password_token(@token)
+    return not_authenticated if @user.blank?
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
+  # allow_mil：対象の値がnilの場合にバリデーションをスキップ
   validates :reset_password_token, uniqueness: true, allow_nil: true
 
   # sorceryの公式ドキュメントに書かれているバリデーション
