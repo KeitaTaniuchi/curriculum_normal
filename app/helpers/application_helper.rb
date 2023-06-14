66ed4ef5dc9fcf7ab7e6a73a7ca3fc1e98b1d504
Keita Taniuchi <barbeekonta1995@gmail.com>
2023-06-14 20:58:20 +0900

    課題22 管理者画面と通常画面のタイトル表示用を切り替えるヘルパーを作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                  'SAMPLE BOARD APP(管理画面)'
+                 else
+                  'SAMPLE BOARD APP'
+                 end
+
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
 end
